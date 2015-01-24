dca537f9a5781f12bf80dc75843945aa5f5f846a
Steven Winston <swinston@mirada.com>
2015-01-24 15:58:28 -0800

    Initial commit

--- platforms/scripts/cmake_android_arm.sh
+++ platforms/scripts/cmake_android_arm.sh
@@ -4,4 +4,4 @@ cd `dirname $0`/..
 mkdir -p build_android_arm
 cd build_android_arm
 
-cmake -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
+cmake -DANDROID_ABI=armeabi-v7a -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake $@ ../..
